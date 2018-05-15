b3b1ff71b332f72c2fa43e53d1e06f3c7aa445cd
John <cooks.johns@gmail.com>
2018-05-15 15:30:48 -0500

    Main Menu is set up nexut

--- src/Contacts.java
+++ src/Contacts.java
@@ -4,9 +4,37 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 
 public class Contacts {
+
+    protected String names;
+    private int phoneNumber;
+
+    public Contacts(String names, int phoneNumber) {
+        this.names = names;
+        this.phoneNumber = phoneNumber;
+
+    }
+
+
+
+
+
+
+
+    public static void main(String[] args){
+
+        System.out.printf("Welcome to my contacts" + " \n" +
+                "<--------------------------------->" + " \n" +
+                "1. View contacts." + " \n" +
+                "2. Add a new contact." + " \n" +
+                "3. Search a contact by name." + " \n" +
+                "4. Delete an existing contact." + " \n" +
+                "5. Exit." + " \n" +
+                "<-------------------------------->");
+
+    }
+
 
 }
